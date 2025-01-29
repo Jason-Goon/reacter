a623a7017032c56744308fe6a1a3ba526be974f6
bossman <bossman@sanaton.media.com>
2025-01-29 03:22:02 +0200

    visttu

--- client/src/vite.config.js
+++ client/src/vite.config.js
@@ -2,16 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/dev/',
     plugins: [react()],
     server: {
-        port: 3006,
-        proxy: {
-            '/dev/api': {
-                target: 'http://localhost:3005',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/dev\/api/, '/dev')
-            }
-        }
+        host: true,
+        port: 3006
     }
 });
