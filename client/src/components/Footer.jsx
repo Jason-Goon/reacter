12fcb17fd25c81ec2c9f1f9736529d7f8ef708cf
bossman <bossman@sanaton.media.com>
2025-01-29 04:54:56 +0200

    vittu

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// client/src/components/Footer.jsx
+function Footer() {
+    return (
+      <footer className="bg-gray-800 text-white mt-auto">
+        <div className="container mx-auto px-6 py-4 text-center">
+          <p>&copy; 2025 MoneyTracker. All rights reserved.</p>
+        </div>
+      </footer>
+    )
+  }
+  
+  export default Footer
+  
