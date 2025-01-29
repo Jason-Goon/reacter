cc9c1a9549fb424fdd404edcf18edac1de4fc734
bossman <bossman@sanaton.media.com>
2025-01-29 04:56:41 +0200

    vittu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,14 +18,5 @@ function Header() {
     )
   }
   
-  // src/components/Footer.jsx
-  function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white mt-auto">
-        <div className="container mx-auto px-6 py-4 text-center">
-          <p>&copy; 2025 MoneyTracker. All rights reserved.</p>
-        </div>
-      </footer>
-    )
-  }
+  export default Header  // Add this line
   
